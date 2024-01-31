d70afa18650d356b4594405aadbf04d26a64e1aa
Ilya <i0treret@gmail.com>
2024-02-01 01:34:15 +0300

    Initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,29 @@
 
 int main()
 {
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
-    window.setFramerateLimit(144);
+    sf::RenderWindow window(sf::VideoMode(800, 800), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    sf::Texture texeture;
+    if (!texeture.loadFromFile("../resources/img.jpg")) {
+        return EXIT_FAILURE;
+    }
+    sf::Sprite sprite(texeture);
 
     while (window.isOpen())
     {
-        for (auto event = sf::Event{}; window.pollEvent(event);)
+        sf::Event event;
+        while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
-            {
                 window.close();
-            }
         }
 
         window.clear();
+        window.draw(sprite);
         window.display();
     }
+
+    return 0;
 }
